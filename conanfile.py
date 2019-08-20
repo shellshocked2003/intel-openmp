934e77995c8e7b83548804f30b554525192fa98e
Michael Gardner <shellshocked2003@gmail.com>
2019-08-19 17:02:44 -0700

    Updated conan config to use Intel OpenMP

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,14 @@
 from conans import ConanFile, CMake, tools
 
 class mklDynamic(ConanFile):
-    name = "openmp"
-    version = "8.0.1"
-    url = "https://github.com/shellshocked2003/openmp"
-    homepage = "https://anaconda.org/anaconda/openmp"
+    name = "intel-openmp"
+    version = "2019.4"
+    url = "https://github.com/shellshocked2003/intel-openmp"
+    homepage = "https://anaconda.org/anaconda/intel-openmp"
     author = "Michael Gardner <mhgardner@berkeley.edu>"
     license = "Intel Simplified Software License"   
     settings = "os", "compiler", "build_type", "arch"
-    description = "OpenMP Shared Libraries"
+    description = "Intel OpenMP Shared Libraries"
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
     build_policy = "missing"
@@ -24,11 +24,11 @@ class mklDynamic(ConanFile):
     
     def build(self):
         if self.settings.os == "Windows":
-            url = ("https://anaconda.org/conda-forge/openmp/8.0.0/download/win-64/openmp-8.0.0-1.tar.bz2")
+            url = ("https://anaconda.org/anaconda/intel-openmp/2019.4/download/win-64/intel-openmp-2019.4-245.tar.bz2")
         elif self.settings.os == "Macos":
-            url = ("https://anaconda.org/conda-forge/openmp/8.0.1/download/osx-64/openmp-8.0.1-0.tar.bz2")
+            url = ("https://anaconda.org/anaconda/intel-openmp/2019.4/download/osx-64/intel-openmp-2019.4-233.tar.bz2")
         elif self.settings.os == "Linux":
-            url = ("https://anaconda.org/conda-forge/openmp/8.0.1/download/linux-64/openmp-8.0.1-0.tar.bz2")
+            url = ("https://anaconda.org/anaconda/intel-openmp/2019.4/download/linux-64/intel-openmp-2019.4-243.tar.bz2")
         else:
             raise Exception("Binary does not exist for these settings")
         tools.get(url, destination=self._source_subfolder)
