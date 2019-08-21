f7eff4a1a02dd5120c51ad1454c3b67fdaaf748a
Michael Gardner <shellshocked2003@gmail.com>
2019-08-21 16:40:40 -0700

    Removed compiler and build type for settings

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class mklDynamic(ConanFile):
     homepage = "https://anaconda.org/anaconda/intel-openmp"
     author = "Michael Gardner <mhgardner@berkeley.edu>"
     license = "Intel Simplified Software License"
-    settings = {"os": None, "compiler": None, "build_type": None, "arch": ["x86_64"]}
+    settings = {"os": None, "arch": ["x86_64"]}
     description = "Intel OpenMP Shared Libraries"
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
